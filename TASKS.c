83b33efb3015d7d8bfb7b38440fbb17236eba2e4
Carolina <cdarocha@web.de>
2025-05-04 14:11:57 -0300

    Fix priority

--- TASKS.c
+++ TASKS.c
@@ -14,11 +14,11 @@ void start_tasks(void) {
     xTaskCreate(task_1, "Task1", 256, NULL, 2, NULL);
     print_line("Created task 1.");
     xTaskCreate(task_2, "Task2", 256, NULL, 2, NULL);
-    print_line("Created task 2.");
+    print_line("Created task 2");
 
-    print_line("Starting Scheduler");
-    print_line("--------------------------------------------------");
     vTaskStartScheduler();
+    print_line("Started Scheduler");
+    print_line("--------------------------------------------------");
 }
 
 void task_1(void *arg)
@@ -40,8 +40,8 @@ void task_2(void *arg)
 void slow_print(const char *msg)
 {
     for (int i = 0; msg[i] != '\0'; i++) {
-        print_character(msg[i]);  // Char-by-char print
-        busy_wait_ms(200); // Artificial delay between chars, Human typing feel, 5 chars/sec
+        print_character(msg[i]);
+        busy_wait_ms(200);
     }
 }
 
@@ -52,6 +52,7 @@ void busy_wait_ms(uint32 ms)
         __nop();  // No-operation to burn CPU cycles
     }
 }
+
 /* Required FreeRTOS callback, called in case of a stack overflow.
  * For the sake of simplicity, this function will loop indefinitely
  * and the root cause can be confirmed by using a debugger
