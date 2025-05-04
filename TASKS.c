dc5cc700668569babff9cdd501f0eda21e4a207d
Carolina <cdarocha@web.de>
2025-05-04 14:04:44 -0300

    created deadlock

--- TASKS.c
+++ TASKS.c
@@ -51,7 +51,6 @@ void task_2(void *arg)
         if (xSemaphoreTake(xMutex, portMAX_DELAY) == pdTRUE)
         {
             slow_print("Greetings from Task 2.\r\n");
-            xSemaphoreGive(xMutex);
         }
         vTaskDelay(pdMS_TO_TICKS(100));
     }
